33bd7fa7870a8e11d3467ccc950e3b81ffd79c66
liyongqiang <1240965061@qq.com>
2018-03-03 22:03:10 +0800

    添加测试ip地址是否可用的方法

--- src/main/java/com/maven/spider/parsehtml/ParseHtml.java
+++ src/main/java/com/maven/spider/parsehtml/ParseHtml.java
@@ -1,7 +1,15 @@
 package com.maven.spider.parsehtml;
 
+import java.io.IOException;
 import java.util.UUID;
 
+import org.apache.http.HttpHost;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -19,6 +27,8 @@ import com.maven.spider.gethtml.GetHtml;
  */
 public class ParseHtml {
 
+	// 浏览器请求头User-Agent
+		final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0";
 	/**
 	 * 分析html页面获取需要的数据
 	 * 
@@ -35,13 +45,14 @@ public class ParseHtml {
 		for (Element element : elements) {
 			// 生成UUID
 			String id = UUID.randomUUID().toString();
-			String ipAddress = element.select("td:eq(1)").toString();
-			String prot = element.select("td:eq(2)").toString();
-			String serverAddress = element.select("td:eq(3)").toString();
-			String isAnonymous = element.select("td:eq(4)").toString();
-			String type = element.select("td:eq(5)").toString();
+			String ipAddress = element.select("td:eq(1)").text();
+			String prot = element.select("td:eq(2)").text();
+			String serverAddress = element.select("td:eq(3)").select("a").text();
+			String isAnonymous = element.select("td:eq(4)").text();
+			String type = element.select("td:eq(5)").text();
 			
 			IP ip = new IP(id, ipAddress, prot, serverAddress, isAnonymous, type);
+			this.getIPIsAvailable(ipAddress, prot, type);
 			jsonArray.add(ip);
 		}
 		// 将json数组转为json字符串
@@ -49,15 +60,52 @@ public class ParseHtml {
 		
 		return jsonStr;
 	}
-	
 	/**
 	 * 判断IP是否可用
 	 * @param ipAdress
+	
 	 * @param port
 	 * @return
 	 */
-	public boolean getIPIsAvailable(String ipAdress,String port) {
-		
+	public boolean getIPIsAvailable(String ipAddress,String prot,String type) {
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		HttpHost proxy = new HttpHost(ipAddress, Integer.valueOf(prot), type);
+		RequestConfig config = RequestConfig.custom().setProxy(proxy).build();
+		HttpGet httpGet = new HttpGet("https://www.baidu.com/");
+		httpGet.setHeader("User-Agent",USER_AGENT);
+		httpGet.setConfig(config);
+		// 获取响应
+		CloseableHttpResponse response = null;
+		try {
+			
+			 response = httpClient.execute(httpGet);
+			
+			// 状态码
+			int statusCode = response.getStatusLine().getStatusCode();
+			if(statusCode == 200){
+				System.out.println(ipAddress + ":" + prot + "=========>可用");
+				return true;
+			}
+			
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			if(response != null){
+                try {
+					response.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+            }
+            if(httpGet != null){
+            	httpGet.abort();
+            }
+		}
 		return false;
 		
 	}
