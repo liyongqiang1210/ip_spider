e5f2c30ab63d05d2616bcc25b4f5af899482bfa1
liyongqiang <liyongqiang@DESKTOP-E4NHH8S>
2018-03-03 17:00:47 +0800

    更新解析页面方法

--- src/main/java/com/maven/spider/parsehtml/ParseHtml.java
+++ src/main/java/com/maven/spider/parsehtml/ParseHtml.java
@@ -1,10 +1,14 @@
 package com.maven.spider.parsehtml;
 
+import java.util.UUID;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import com.alibaba.fastjson.JSONArray;
+import com.maven.spider.entity.IP;
 import com.maven.spider.gethtml.GetHtml;
 
 /**
@@ -22,25 +26,28 @@ public class ParseHtml {
 	 * @return
 	 */
 	public String getData(String content) {
+		// 创建json数组对象
+		JSONArray jsonArray = new JSONArray();
 		// 将content转化为doc文档
 		Document doc = Jsoup.parse(content);
 		//
 		Elements elements = doc.select("tr.odd");
 		for (Element element : elements) {
-			
+			// 生成UUID
+			String id = UUID.randomUUID().toString();
 			String ipAddress = element.select("td:eq(1)").toString();
 			String prot = element.select("td:eq(2)").toString();
 			String serverAddress = element.select("td:eq(3)").toString();
 			String isAnonymous = element.select("td:eq(4)").toString();
 			String type = element.select("td:eq(5)").toString();
-
+			
+			IP ip = new IP(id, ipAddress, prot, serverAddress, isAnonymous, type);
+			jsonArray.add(ip);
 		}
-		return content;
-	}
-
-	public static void main(String[] args) {
-		GetHtml gh = new GetHtml();
-		new ParseHtml().getData(gh.get("http://www.xicidaili.com/nn/"));
+		// 将json数组转为json字符串
+		String jsonStr = jsonArray.toJSONString(); 
+		
+		return jsonStr;
 	}
 
 }
