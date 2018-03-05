52aed6553bb92f013b112040286f09dc98dfab1c
liyongqiang <1240965061@qq.com>
2018-03-05 20:11:40 +0800

    更新解析页面类

--- src/main/java/com/maven/spider/parsehtml/ParseHtml.java
+++ src/main/java/com/maven/spider/parsehtml/ParseHtml.java
@@ -1,24 +1,13 @@
 package com.maven.spider.parsehtml;
 
-import java.io.IOException;
-import java.util.UUID;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.apache.http.HttpHost;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
-import com.alibaba.fastjson.JSONArray;
 import com.maven.spider.entity.IP;
-import com.maven.spider.gethtml.GetHtml;
-import com.maven.spider.testip.TestIP;
 
 /**
  * 使用Jsoup解析页面
@@ -35,43 +24,30 @@ public class ParseHtml {
 	 * @return
 	 * @throws Exception
 	 */
-	public String getJsonData(String content) {
-		// 创建json数组对象
-		JSONArray jsonArray = new JSONArray();
+	public List<IP> getIPList(String content) {
+
+		// 创建list集合用来存储ip对象
+		List<IP> list = new ArrayList<IP>();
 		// 将content转化为doc文档
 		Document doc = Jsoup.parse(content);
-		//
+		// 获取高匿的ip地址
 		Elements elements = doc.select("tr.odd");
+		// 遍历匹配到的信息
 		for (Element element : elements) {
-			// 生成UUID
-			String id = UUID.randomUUID().toString();
-			String ipAddress = element.select("td:eq(1)").text();
-			String prot = element.select("td:eq(2)").text();
-			String serverAddress = element.select("td:eq(3)").select("a").text();
-			String isAnonymous = element.select("td:eq(4)").text();
-			String type = element.select("td:eq(5)").text();
 
-			IP ip = new IP(id, ipAddress, prot, serverAddress, isAnonymous, type);
-
-			boolean ipIsAvailable = new TestIP().getIPIsAvailable(ipAddress, prot, type);
-
-			System.out.println("ip地址：" + ipAddress + ",测试结果：" + ipIsAvailable);
+			// 匹配我们需要的标签
+			String ip_address = element.select("td:eq(1)").text();
+			String ip_prot = element.select("td:eq(2)").text();
+			String ip_server_address = element.select("td:eq(3)").select("a").text();
+			String ip_is_anonymous = element.select("td:eq(4)").text();
+			String ip_type = element.select("td:eq(5)").text();
 			
-			if (ipIsAvailable) {
-				jsonArray.add(ip);
-			}
-
+			// 创建带参数的ip对象
+			IP ip = new IP(ip_address, ip_prot, ip_server_address, ip_is_anonymous, ip_type);
+			list.add(ip);
 		}
-		// 将json数组转为json字符串
-		String jsonStr = jsonArray.toJSONString();
-
-		return jsonStr;
-	}
-
-	public static void main(String[] args) {
-		String string = new GetHtml().get("http://www.xicidaili.com/nn/");
-		String jsonData = new ParseHtml().getJsonData(string);
-		System.out.println(jsonData);
+		
+		return list;
 	}
 
 }
