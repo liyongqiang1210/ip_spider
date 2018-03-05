70aaf0285994b4ea0f8cb5cb4b85de8205dcdce2
liyongqiang <1240965061@qq.com>
2018-03-05 20:15:15 +0800

    添加啊爬虫主函数

---
+++ src/main/java/com/maven/spider/main/SpiderMain.java
@@ -0,0 +1,54 @@
+package com.maven.spider.main;
+
+import java.util.List;
+
+import com.maven.spider.entity.IP;
+import com.maven.spider.gethtml.GetHtml;
+import com.maven.spider.jdbc.DBUtils;
+import com.maven.spider.parsehtml.ParseHtml;
+import com.maven.spider.testip.TestIP;
+
+/**
+ * 爬虫程序入口
+ * 
+ * @author liyongqiang
+ *
+ */
+public class SpiderMain {
+
+	public static void main(String[] args) {
+		GetHtml gh = new GetHtml();
+		ParseHtml ph = new ParseHtml();
+		DBUtils db = new DBUtils();
+		TestIP test = new TestIP();
+
+		for (int i = 0; i < 2000; i++) {
+
+			// 设置url
+			String url = "http://www.xicidaili.com/nn/" + i;
+			// 获取网页内容
+			String content = gh.get(url);
+			// 获取json格式的ip数据列表
+			List<IP> list = ph.getIPList(content);
+			for(IP ip:list){
+				String ip_address = ip.getIp_address();
+				String ip_prot = ip.getIp_prot();
+				String ip_type = ip.getIp_type();
+				
+				// 查询ip是否可用
+				boolean ipIsAvailable = test.getIPIsAvailable(ip_address, ip_prot, ip_type);
+				 
+				// 如果可用那么将其Ip_is_user属性设置为1
+				if(ipIsAvailable){
+					ip.setIp_is_user(1);
+				}else{
+					ip.setIp_is_user(0);
+				}
+				
+				// 插入数据库
+				db.insertIP(ip);
+				
+			}
+		}
+	}
+}
