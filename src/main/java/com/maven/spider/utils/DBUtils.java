bb2fa5a598dd4cce6d0ec9cfc8022a175dffaf68
liyongqiang <1240965061@qq.com>
2018-03-06 19:40:14 +0800

    添加数据库操作工具类与时间操作工具类

---
+++ src/main/java/com/maven/spider/utils/DBUtils.java
@@ -0,0 +1,65 @@
+package com.maven.spider.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * 数据库工具类
+ * @author Li Yongqiang
+ *
+ */
+public class DBUtils {
+
+	public static final String URL = "jdbc:mysql://localhost:3306/maven";
+	public static final String USER = "root";
+	public static final String PASSWORD = "root";
+
+	/**
+	 * 获取数据库连接
+	 * @return
+	 */
+	public static Connection getConnection() {
+		Connection conn = null;
+		try {
+			// 获取数据库驱动
+			Class.forName("com.mysql.jdbc.Driver");
+			// 获取连接
+			conn = DriverManager.getConnection(URL, USER, PASSWORD);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return conn;
+
+	}
+
+	/**
+	 * 关闭连接的方法
+	 * @param rs
+	 * @param ps
+	 * @param conn
+	 */
+	public static void close(ResultSet rs, PreparedStatement ps, Connection conn) {
+		// 关闭数据库连接
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			if (ps != null) {
+				ps.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/main/java/com/maven/spider/utils/DateUtils.java
@@ -0,0 +1,20 @@
+package com.maven.spider.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtils {
+
+	private static final Date DATE = new Date();
+
+	/**
+	 * 获取年-月-日 时:分:秒格式的数据字符串
+	 * @return
+	 */
+	public static String getYMDHMS() {
+		SimpleDateFormat sdf = new SimpleDateFormat("YYYY-mm-dd HH:mm:ss");
+		String date = sdf.format(DATE);
+		return date;
+	}
+
+}
