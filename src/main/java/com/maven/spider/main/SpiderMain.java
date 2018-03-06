595f4474b07fe7435a4edccafca9a1a1dfa111ad
liyongqiang <1240965061@qq.com>
2018-03-06 21:12:50 +0800

    更新爬虫入口程序逻辑

--- src/main/java/com/maven/spider/main/SpiderMain.java
+++ src/main/java/com/maven/spider/main/SpiderMain.java
@@ -3,8 +3,8 @@ package com.maven.spider.main;
 import java.util.List;
 
 import com.maven.spider.entity.IP;
-import com.maven.spider.gethtml.GetHtml;
-import com.maven.spider.jdbc.DBUtils;
+import com.maven.spider.gethtml.HttpClientRequest;
+import com.maven.spider.jdbc.JDBC;
 import com.maven.spider.parsehtml.ParseHtml;
 import com.maven.spider.testip.TestIP;
 
@@ -17,12 +17,12 @@ import com.maven.spider.testip.TestIP;
 public class SpiderMain {
 
 	public static void main(String[] args) throws InterruptedException {
-		GetHtml gh = new GetHtml();
+		HttpClientRequest gh = new HttpClientRequest();
 		ParseHtml ph = new ParseHtml();
-		DBUtils db = new DBUtils();
+		JDBC db = new JDBC();
 		TestIP test = new TestIP();
 
-		for (int i = 1; i < 20; i++) {
+		for (int i = 1; i < 2000; i++) {
 
 			// 设置url
 			String url = "http://www.xicidaili.com/nn/" + i;
@@ -30,25 +30,23 @@ public class SpiderMain {
 			String content = gh.get(url);
 			// 获取json格式的ip数据列表
 			List<IP> list = ph.getIPList(content);
-			System.out.println(list);
-			for(IP ip:list){
+			for (IP ip : list) {
 				String ip_address = ip.getIp_address();
 				String ip_prot = ip.getIp_prot();
 				String ip_type = ip.getIp_type();
-				
+
 				// 查询ip是否可用
 				boolean ipIsAvailable = test.getIPIsAvailable(ip_address, ip_prot, ip_type);
-				 
+
 				// 如果可用那么将其Ip_is_user属性设置为1
-				if(ipIsAvailable){
+				if (ipIsAvailable) {
 					ip.setIp_is_user(1);
+					// 插入数据库
+					db.insertIP(ip);
 				}else{
-					ip.setIp_is_user(0);
+					System.out.println(ip_address + "===========>不可用！");
 				}
-				
-				// 插入数据库
-//				db.insertIP(ip);
-				
+
 			}
 		}
 	}
