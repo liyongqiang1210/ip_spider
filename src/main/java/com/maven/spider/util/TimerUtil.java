f8f7414f5bd481735a3029fba9934578cc46af57
liyongqiang <1240965061@qq.com>
2018-03-09 12:47:27 +0800

    添加一个定时器工具类

---
+++ src/main/java/com/maven/spider/util/TimerUtil.java
@@ -0,0 +1,32 @@
+package com.maven.spider.util;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * 定时器工具类
+ * @author Li Yongqiang
+ *
+ */
+public class TimerUtil {
+
+	// 创建定时器Timer对象
+	private static final Timer timer = new Timer();
+	
+	public static void main(String[] args){
+		
+		// 启动后延迟时间
+		long afterSs = 10*1000;
+		// 执行周期
+		long intervalSsl = 1*1000;
+		timer.schedule(new TimerTask() {
+			// 执行次数计数器
+			int i = 0;
+			@Override
+			public void run() {
+				System.out.println("定时器执行次数：" + i++);
+			}
+		}, afterSs,intervalSsl);
+	}
+	
+}
