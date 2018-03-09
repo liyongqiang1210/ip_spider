0f84ecc85e17bc88efad9f31e23fad98a0c5d677
liyongqiang <1240965061@qq.com>
2018-03-09 12:54:17 +0800

    修复getYMDHMS()方法返回时间不变的bug

--- src/main/java/com/maven/spider/util/DateUtil.java
+++ src/main/java/com/maven/spider/util/DateUtil.java
@@ -5,15 +5,13 @@ import java.util.Date;
 
 public class DateUtil {
 
-	private static final Date DATE = new Date();
-
 	/**
 	 * 获取年-月-日 时:分:秒格式的数据字符串
 	 * @return
 	 */
 	public static String getYMDHMS() {
-		SimpleDateFormat sdf = new SimpleDateFormat("YYYY-mm-dd HH:mm:ss");
-		String date = sdf.format(DATE);
+		SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss");
+		String date = sdf.format(new Date());
 		return date;
 	}
 
