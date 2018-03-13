41728a9a2eee441cf2f1aa70f635ad6f61f3a0b5
liyongqiang <1240965061@qq.com>
2018-03-13 20:44:22 +0800

    创建csdn爬虫入口

--- src/main/java/com/maven/spider/main/CsdnMain.java
+++ src/main/java/com/maven/spider/main/CsdnMain.java
@@ -1,12 +1,17 @@
 package com.maven.spider.main;
 
+import java.util.List;
+
+import com.maven.spider.entity.CSDN;
 import com.maven.spider.httpclient.HttpClientRequest;
+import com.maven.spider.parser.ParserCsdn;
 
 public class CsdnMain {
 
 	public static void main(String[] args) {
 		HttpClientRequest hcq = new HttpClientRequest();
 		String content = hcq.get("https://www.csdn.net/nav/newarticles");
-		System.out.println(content);
+		List<CSDN> csdnArcitleList = ParserCsdn.getCsdnArcitleList(content, "最新文章");
+		System.out.println(csdnArcitleList);
 	}
 }
