a0d4e8a0633b7c4ca21de477ddbe1f6ebaba2687
liyongqiang <1240965061@qq.com>
2018-03-14 12:45:44 +0800

    添加定时器

--- src/main/java/com/maven/spider/main/CsdnMain.java
+++ src/main/java/com/maven/spider/main/CsdnMain.java
@@ -1,24 +1,44 @@
 package com.maven.spider.main;
 
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import com.maven.spider.entity.CSDN;
 import com.maven.spider.httpclient.HttpClientRequest;
 import com.maven.spider.jdbc.CsdnJdbc;
 import com.maven.spider.parser.CsdnParser;
+import com.maven.spider.util.DateUtil;
 
 public class CsdnMain {
 
+	// 创建定时器Timer对象
+	private static final Timer timer = new Timer();
+	// 创建模拟浏览器请求对象
+	private final static HttpClientRequest hcq = new HttpClientRequest();
+
 	public static void main(String[] args) {
-		HttpClientRequest hcq = new HttpClientRequest();
-		String content = hcq.get("https://www.csdn.net/nav/newarticles");
-		List<CSDN> csdnArcitleList = CsdnParser.getCsdnArcitleList(content, "最新文章");
-		for (CSDN csdn : csdnArcitleList) {
-			String url = csdn.getUrl();
-			boolean selectCsdn = CsdnJdbc.selectCsdn(url);
-			if(selectCsdn){
-				CsdnJdbc.insertCsdn(csdn);
+		// 启动后延迟时间
+		long afterSs = 1 * 1000;
+		// 执行周期
+		long intervalSsl = 1 * 10000;
+		timer.schedule(new TimerTask() {
+			// 执行次数计数器
+			int i = 0;
+
+			@Override
+			public void run() {
+				System.out.println("当前时间：" + DateUtil.getYMDHMS() + "定时器执行次数：" + i++);
+				String content = hcq.get("https://www.csdn.net/nav/newarticles");
+				List<CSDN> csdnArcitleList = CsdnParser.getCsdnArcitleList(content, "最新文章");
+				for (CSDN csdn : csdnArcitleList) {
+					String url = csdn.getUrl();
+					boolean selectCsdn = CsdnJdbc.selectCsdn(url);
+					if (selectCsdn) {
+						CsdnJdbc.insertCsdn(csdn);
+					}
+				}
 			}
-		}
+		}, afterSs, intervalSsl);
 	}
 }
