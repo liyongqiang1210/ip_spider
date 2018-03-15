0662910593dacc27f90adf303f9249867165fe40
liyongqiang <1240965061@qq.com>
2018-03-15 19:45:59 +0800

    添加获取博客园文章url的方法

--- src/main/java/com/maven/spider/parser/ArticleParser.java
+++ src/main/java/com/maven/spider/parser/ArticleParser.java
@@ -18,6 +18,8 @@ import com.maven.spider.entity.Article;
  */
 public class ArticleParser {
 
+	private static final List<Article> list = new ArrayList<Article>();
+
 	/**
 	 * 获取csdn文章列表的方法
 	 * 
@@ -26,8 +28,7 @@ public class ArticleParser {
 	 * @param source
 	 * @return
 	 */
-	public static List<Article> getCsdnArcitleList(String content, String articleType,String source) {
-		List<Article> list = new ArrayList<Article>();
+	public static List<Article> getCsdnArcitleList(String content, String articleType, String source) {
 		// 将网页内容转换成doc格式
 		Document doc = Jsoup.parse(content);
 		Elements elements = doc.select("ul.feedlist_mod>li.clearfix");
@@ -41,7 +42,27 @@ public class ArticleParser {
 			Article csdn = new Article(url, title, author, createTime, source, articleType);
 			list.add(csdn);
 		}
-		
+
 		return list;
+	}
+
+	public static List<Article> getCnblogsList(String content, String articleType, String source) {
+
+		Document doc = Jsoup.parse(content);
+		Elements elements = doc.select("div.post_item_body");
+		for (Element element : elements) {
+
+			String title = element.select("h3>a.titlelnk").text();// 获取文章标题
+			String url = element.select("h3>a").attr("href");// 获取文章标题
+			String author = element.select("a.lightblue").text();// 获取文章作者
+			String createTime = element.select("div.post_item_foot").text();// 获取文章创建时间
+
+			// 创建csdn对象
+			Article csdn = new Article(url, title, author, createTime, source, articleType);
+			list.add(csdn); 
+			
+		}
+		return list;
+
 	}
 }
