8b06876e468dbd84a73a7c0eda6142c272476b5c
liyongqiang <1240965061@qq.com>
2018-03-19 18:50:27 +0800

    添加新闻jdbc类

---
+++ src/main/java/com/maven/spider/jdbc/NewsJdbc.java
@@ -0,0 +1,66 @@
+package com.maven.spider.jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.maven.spider.entity.News;
+import com.maven.spider.util.DBUtil;
+
+/**
+ * 新闻url DB类
+ * 
+ * @author Li Yongqiang
+ *
+ */
+public class NewsJdbc {
+
+	/**
+	 * 判断这条新闻url是否存在的方法
+	 * 
+	 * @param newsUrl
+	 * @return
+	 */
+	public static boolean getNewsUrlIsExit(String newsUrl) {
+
+		Connection conn = DBUtil.getConnection();
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		String sql = "select * from news_list where news_url = ?";
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, newsUrl);
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				return true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public static void insertNewsUrl(News news) {
+
+		Connection conn = DBUtil.getConnection();
+		PreparedStatement ps = null;
+		String sql = "insert into news_list(news_id,news_title,news_url,news_source_website,news_type,news_release_time) "
+				+ "values(?,?,?,?,?,?)";
+		try {
+			ps = conn.prepareStatement(sql);
+			ps.setString(1, news.getNewsId());
+			ps.setString(2, news.getNewsTitle());
+			ps.setString(3, news.getNewsUrl());
+			ps.setString(4, news.getNewsSourceWebsite());
+			ps.setString(5, news.getNewsType());
+			ps.setString(6, news.getNewsReleaseTime());
+			int isSuccess = ps.executeUpdate();
+			if(isSuccess == 1){
+				System.out.println(news.getNewsUrl() + "================>添加成功！");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
