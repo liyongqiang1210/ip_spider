1630f633398fca7d2b40e0c4a6e88e1582c70166
liyongqiang <1240965061@qq.com>
2018-03-19 18:52:42 +0800

    更新新闻的解析器

--- src/main/java/com/maven/spider/parser/NewsJsoupParser.java
+++ src/main/java/com/maven/spider/parser/NewsJsoupParser.java
@@ -22,31 +22,86 @@ public class NewsJsoupParser {
 
 	// 获取今天的日期
 	private static final String dateTime = DateUtil.getYMD();
-	
+
 	/**
 	 * 获取当天最新新闻url列表
+	 * 
 	 * @param content
 	 * @param newsSourceWebsite
 	 * @param newsType
 	 * @return
 	 */
-	public static List<News> getNewsList(String content,String newsSourceWebsite,String newsType){
-		
+	public static List<News> getNewsList(String content, String newsSourceWebsite, String newsType) {
+
 		List<News> list = new ArrayList<News>();
 		Document doc = Jsoup.parse(content);
-		Elements elements = doc.select("ul.list_show>li"); // 获取新闻列表集合
-		for(Element element:elements){
-			String newsId = UUID.randomUUID().toString(); // 获取新闻id
-			String newsTitle = element.select("h2>a").text(); // 获取新闻title 
-			String newsUrl = element.select("h2>a").attr("href"); // 获取新闻url
-			String newsReleaseTime = element.select("div.time").text(); // 获取新闻发布时间
-		
-			if(newsReleaseTime.equals(dateTime)){// 当发布日期等于今天日期的时候进入
+		if (newsType.equals("军事")) {
+			
+			Elements elements = doc.select("ul.list_show>li"); // 获取新闻列表集合
+			
+			for (Element element : elements) {
+				
+				String newsId = UUID.randomUUID().toString(); // 获取新闻id
+				String newsTitle = element.select("h2>a").text(); // 获取新闻title
+				String newsUrl = element.select("h2>a").attr("href"); // 获取新闻url
+				String newsReleaseTime = element.select("div.time").text(); // 获取新闻发布时间
 
-				News news = new News(newsId, newsTitle, newsUrl, newsSourceWebsite, newsType, newsReleaseTime);
-				list.add(news);
+				if (newsReleaseTime.equals(dateTime)) {// 当发布日期等于今天日期的时候进入
+
+					News news = new News(newsId, newsTitle, newsUrl, newsSourceWebsite, newsType, newsReleaseTime);
+					list.add(news);
+				}
+			}
+		} else if (newsType.equals("国际") || newsType.equals("房产") || newsType.equals("科技")) {
+			
+			
+			// 获取新闻列表集合
+			Elements elements = doc.select("ul.dataList>li");
+			for (Element element : elements) {
+				String newsId = UUID.randomUUID().toString(); // 获取新闻id
+				String newsTitle = element.select("h3>a").text(); // 获取新闻title
+				String newsUrl = element.select("h3>a").attr("href"); // 获取新闻url
+				String newsReleaseTime = element.select("span.time").text(); // 获取新闻发布时间
+
+				if (newsReleaseTime.equals(dateTime)) {// 当发布日期等于今天日期的时候进入
+
+					News news = new News(newsId, newsTitle, newsUrl, newsSourceWebsite, newsType, newsReleaseTime);
+					list.add(news);
+				}
+			}
+			
+			// 获取房产滚动新闻集合
+			Elements gd_elements = doc.select("ul#gd_content>li"); 
+			
+			for (Element element : gd_elements) {
+				String newsId = UUID.randomUUID().toString(); // 获取新闻id
+				String newsTitle = element.select("li>a").text(); // 获取新闻title
+				String newsUrl = element.select("li>a").attr("href"); // 获取新闻url
+				String newsReleaseTime = DateUtil.getYMD(); // 获取今天的日期 
+				
+				if(newsUrl.matches(".*" + newsReleaseTime + ".*")){ // 根据url地址判断是否是今天的新闻
+					
+					News news = new News(newsId, newsTitle, newsUrl, newsSourceWebsite, newsType, newsReleaseTime);
+					list.add(news);
+				}
+			}
+			
+			// 获取科技新闻集合
+			Elements kj_element = doc.select("a");
+			for (Element element : kj_element) {
+				
+				String newsUrl = element.select("a").attr("href");
+				String newsReleaseTime = DateUtil.getYMD();
+				
+				if(newsUrl.matches(".*" + newsReleaseTime.substring(0,7) + "/" + newsReleaseTime.substring(8) + ".*")){
+
+					String newsId = UUID.randomUUID().toString(); // 获取新闻id
+					String newsTitle = element.select("a").text();
+					System.out.println(newsTitle + " ============> " + newsUrl);
+				}
 			}
 		}
+
 		return list;
 	}
 }
