b0bc6d18bf9aed31d9399846aafa7bff19609569
liyongqiang <1240965061@qq.com>
2018-03-26 11:31:17 +0800

    为get方法添加coding参数值

--- src/main/java/com/maven/spider/main/NewsMain.java
+++ src/main/java/com/maven/spider/main/NewsMain.java
@@ -3,36 +3,36 @@ package com.maven.spider.main;
 import java.util.List;
 
 import com.maven.spider.entity.News;
+import com.maven.spider.entity.UrlList;
 import com.maven.spider.httpclient.HttpClientRequest;
 import com.maven.spider.jdbc.NewsJdbc;
+import com.maven.spider.jdbc.NewsUrlJdbc;
 import com.maven.spider.parser.NewsJsoupParser;
 
 /**
  * 新闻爬虫入口主函数
+ * 
  * @author Li Yongqiang
  *
  */
 public class NewsMain {
 
-	
-	
-	
 	static final HttpClientRequest hcq = new HttpClientRequest();
-	
-	public static void main(String[] args){
-		//String url = "http://www.xinhuanet.com/mil/index.htm"; // 军事
-		//String url = "http://www.xinhuanet.com/world/index.htm"; // 国际
-		//String url = "http://www.news.cn/house/index.htm"; // 房产
-		//String url = "http://www.xinhuanet.com/tech/index.htm"; // 科技
-		//String url = "http://ent.news.cn/"; // 娱乐
-		//String url = "http://sports.xinhuanet.com/"; // 体育
-		String url = "http://www.news.cn/auto/"; // 汽车
-		String content = hcq.get(url);
-		List<News> newsList = NewsJsoupParser.getNewsList(content, "新华网", "汽车");
-		for(News news: newsList){
-			boolean isExit = NewsJdbc.getNewsUrlIsExit(news.getNewsUrl());
-			if(!isExit){
-				NewsJdbc.insertNewsUrl(news);
+
+	public static void main(String[] args) {
+		
+		List<UrlList> urlList = NewsUrlJdbc.getUrlList("中新网");
+		
+		for (UrlList ul : urlList) {
+
+			String content = hcq.get(ul.getNewsUrl(),"gb2312");
+			List<News> newsList = NewsJsoupParser.getNewsList(content, ul.getNewsUrlSource(), ul.getNewsType());
+			
+			for (News news : newsList) {
+				boolean isExit = NewsJdbc.getNewsUrlIsExit(news.getNewsUrl());
+				if (!isExit) {
+					NewsJdbc.insertNewsUrl(news);
+				}
 			}
 		}
 	}
