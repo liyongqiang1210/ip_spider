449a5120d0bd550092f4e1ca6c490f69a3a5fe10
liyongqiang <1240965061@qq.com>
2018-03-29 21:48:24 +0800

    。。

--- src/main/java/com/maven/spider/main/baidu/BaiJiaMain.java
+++ src/main/java/com/maven/spider/main/baidu/BaiJiaMain.java
@@ -1,5 +1,16 @@
 package com.maven.spider.main.baidu;
 
+import java.util.Vector;
+
+import com.maven.spider.entity.BaiJiaHao;
+import com.maven.spider.httpclient.HttpClientRequest;
+import com.maven.spider.parser.baidu.BaiJiaJsoupParser;
+
 public class BaiJiaMain {
 
+	public static void main(String[] args) {
+		
+		String content = HttpClientRequest.get("https://baijia.baidu.com/channel?cat=1", "utf-8");
+		Vector<BaiJiaHao> baiJiaListData = BaiJiaJsoupParser.getBaiJiaListData(content);
+	}
 }
