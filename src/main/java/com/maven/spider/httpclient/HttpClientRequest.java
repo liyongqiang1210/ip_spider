f300a9497063634d1bc8f8c8210246cf312406de
liyongqiang <1240965061@qq.com>
2018-04-01 13:12:09 +0800

    添加百度推荐新闻的post方法

--- src/main/java/com/maven/spider/httpclient/HttpClientRequest.java
+++ src/main/java/com/maven/spider/httpclient/HttpClientRequest.java
@@ -1,13 +1,19 @@
 package com.maven.spider.httpclient;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 /**
@@ -55,6 +61,62 @@ public class HttpClientRequest {
 				if (statusCode == 200) {
 					// 获取网页内容
 					content = EntityUtils.toString(entity, coding);
+				}
+			} finally {
+				res.close();
+			}
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			// 关闭链接，释放资源
+			try {
+				httpClient.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return content;
+	}
+
+	/**
+	 * post请求获取百度推荐新闻需传入参数新闻url以及新闻类型type返回的是json字符串
+	 * 
+	 * @param url
+	 * @param type
+	 * @return
+	 */
+	public static String baiDuTuiJianPost(String url, String type) {
+		// 创建HttpClient对象
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		// 响应的内容
+		String content = null;
+		try {
+			// 将请求参数追加到url后面
+			String str = "&mid=03c7a16f2e8028127e42c5f7ca9e210b&ts=0&topic=" + type + "&type=info&"
+					+ "token=info&ln=200&an=10&withtopic=0&wf=0&internet-subscribe=0&ver=4&pd=webapp&nids="
+					+ "&remote_device_type=1&os_type=1&screen_size_width=1920&screen_size_height=1080";
+			// 创建post请求
+			HttpPost req = new HttpPost(url + str);
+			// 设置请求头header
+			req.setHeader("User-Agent", USER_AGENT);
+			req.setHeader("Accept", ACCEPT);
+			req.setHeader("Content-Type", CONTENT_TYPE);
+			// 发送post请求
+			CloseableHttpResponse res = httpClient.execute(req);
+			try {
+				// 获取响应实体
+				HttpEntity entity = res.getEntity();
+
+				// 响应状态
+				int statusCode = res.getStatusLine().getStatusCode();
+				System.out.println("Code：" + statusCode);
+
+				// 如果返回200则证明请求成功
+				if (statusCode == 200) {
+					// 获取网页内容
+					content = EntityUtils.toString(entity);
 				}
 			} finally {
 				res.close();
