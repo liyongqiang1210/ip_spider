04d0a29a57b652a3947b0e5b35e23811b28822a2
liyongqiang <1240965061@qq.com>
2018-04-02 14:08:04 +0800

    添加线程

--- src/main/java/com/maven/spider/main/NewsMain.java
+++ src/main/java/com/maven/spider/main/NewsMain.java
@@ -10,29 +10,46 @@ import com.maven.spider.jdbc.NewsUrlJdbc;
 import com.maven.spider.parser.NewsJsoupParser;
 
 /**
- * 新闻爬虫入口主函数
- * 会者不难，难者不会
+ * 新闻爬虫入口主函数 会者不难，难者不会
+ * 
  * @author Li Yongqiang
  *
  */
 public class NewsMain {
 
-
 	public static void main(String[] args) {
-		
-		List<UrlList> urlList = NewsUrlJdbc.getUrlList("中新网");
-		
-		for (UrlList ul : urlList) {
 
-			String content = HttpClientRequest.get(ul.getNewsUrl(),"gb2312");
-			List<News> newsList = NewsJsoupParser.getNewsList(content, ul.getNewsUrlSource(), ul.getNewsType());
-			
-			for (News news : newsList) {
-				boolean isExit = NewsJdbc.getNewsUrlIsExit(news.getNewsUrl());
-				if (!isExit) {
-					NewsJdbc.insertNewsUrl(news);
+		Thread zxwt = new Thread(new ZhongXinWangThread(), "zhongxinwangThread");
+		zxwt.start();
+	}
+
+}
+
+class ZhongXinWangThread implements Runnable {
+
+	public void run() {
+		while (true) {
+			try {
+				List<UrlList> urlList = NewsUrlJdbc.getUrlList("中新网");
+
+				for (UrlList ul : urlList) {
+
+					String content = HttpClientRequest.get(ul.getNewsUrl(), "gb2312");
+					List<News> newsList = NewsJsoupParser.getNewsList(content, ul.getNewsUrlSource(), ul.getNewsType());
+
+					for (News news : newsList) {
+						boolean isExit = NewsJdbc.getNewsUrlIsExit(news.getNewsUrl());
+						if (!isExit) {
+							NewsJdbc.insertNewsUrl(news);
+						}
+					}
 				}
+				Thread.sleep(100000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 		}
 	}
+
 }
